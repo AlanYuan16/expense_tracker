af129ea52d26d287ee043a21264297cf0a69d076
alanyuan16 <alanyuan69@gmail.com>
2025-10-09 12:18:37 -0400

    Inital commit

---
+++ lib/expenses.dart
@@ -0,0 +1,36 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget{
+  const Expenses({super.key});
+
+  State<Expenses> createState(){
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'apple', amount: 12.45, date: DateTime.now(), category: Category.food
+      ),
+        Expense(
+      title: 'ticket', amount: 19.41, date: DateTime.now(), category: Category.leisure
+      ),
+
+
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Column(
+        children: [
+          Text("chart"),
+          ExpensesList(expenses: _registeredExpenses)
+          ],
+      )
+    );
+  }
+}
