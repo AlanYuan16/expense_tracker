a8c06e056fb9d9239ef8ddb43c6c7c50d97c6f3d
alanyuan16 <alanyuan69@gmail.com>
2025-11-04 11:15:46 -0500

    Finished making widgets dismissible. Added undo snackbar to retrieve on accidently delete.

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,42 +1,101 @@
 import 'package:expense_tracker/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/newExpense.dart';
 import 'package:flutter/material.dart';
 
-class Expenses extends StatefulWidget{
+class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
-  State<Expenses> createState(){
+  State<Expenses> createState() {
     return _ExpensesState();
   }
 }
 
 class _ExpensesState extends State<Expenses> {
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
+      isScrollControlled: true,
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    int expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Expense Deleted'),
+        duration: Duration(seconds: 3),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
+  }
 
   final List<Expense> _registeredExpenses = [
     Expense(
-      title: 'apple', amount: 12.45, date: DateTime.now(), category: Category.food
-      ),
-        Expense(
-      title: 'ticket', amount: 19.41, date: DateTime.now(), category: Category.leisure
-      ),
-
-
+      title: 'Cheeseburger',
+      amount: 12.45,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: 'Movie Ticket',
+      amount: 18.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'Train ticket',
+      amount: 34.00,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
   ];
+
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text("Click the + button to add an Expense!"),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
     return Scaffold(
       appBar: AppBar(
-        title: const Text ('Expense Tracker'),
+        title: const Text('Expense Tracker'),
         actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.add))
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
         ],
       ),
-      body:Column(
+      body: Column(
         children: [
-          Text("chart"),
-          Expanded(child: ExpensesList(expenses:  _registeredExpenses))
-          ],
-      )
+          Text("Chart"),
+          Expanded(child: mainContent),
+        ],
+      ),
     );
   }
 }
